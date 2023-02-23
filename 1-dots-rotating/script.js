ecc6dd5c719c1c626f1aa68eb7926d38329017ae
Samuel <s.schanderl@tutanota.com>
2023-02-23 16:47:35 +0100

    adjustments to animation 1

--- 1-dots-rotating/script.js
+++ 1-dots-rotating/script.js
@@ -1,13 +1,38 @@
 const button = document.querySelector('.button');
 const loadingBox = document.querySelector('.loading-box');
 
+let isLoading = false;
+
 button.addEventListener('click', () => {
     console.log('clicked');
+    
+    if (!isLoading) {
+        startLoadingAnimation();
+    } else if (isLoading) {
+        stopLoadingAnimation();
+    }
+    changeButtonText();
+})
+
+function startLoadingAnimation() {
+    isLoading = true;
+
     loadingBox.style.display = 'grid';
     loadingBox.classList.add('rotate');
-    loadingBox.classList.add('fade-in');
-    setTimeout(() => {
-        loadingBox.classList.remove('rotate');
-        loadingBox.style.display = 'none';
-    },4000)
-})
+}
+
+function stopLoadingAnimation() {
+
+    isLoading = false;
+
+    loadingBox.classList.remove('rotate');
+    loadingBox.style.display = 'none';
+}
+
+function changeButtonText() {
+    if (isLoading) {
+        button.textContent = 'Stop animation';
+    } else if (!isLoading) {
+        button.textContent = 'Start animation';
+    }
+}
